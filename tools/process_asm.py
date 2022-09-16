821445e46143dbfd1fd0d851ba99397304f6b804
AltimorTASDK <altimormc@gmail.com>
2022-09-15 20:03:31 -0400

    manual magic number, this needs to be fixed

--- tools/process_asm.py
+++ tools/process_asm.py
@@ -112,7 +112,7 @@ def asm_generator(asm, name, symbols):
             if reg is None:
                 error(name, n, "Encountered @sda21 before .set gecko.pic_reg")
 
-            yield raw_line.replace("@sda21(0)", f"+0x800C@sdarel({reg})")
+            yield raw_line.replace("@sda21(0)", f"+0x8030@sdarel({reg})")
             continue
 
         yield raw_line
